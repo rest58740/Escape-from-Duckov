34bbf3de8a9ce553963dcae27007c02047292938
rest58740 <39267517+rest58740@users.noreply.github.com>
2026-02-07 21:24:52 +0800

    Add files via upload

---
+++ UniTask/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: InternalsVisibleTo("UniTask.Linq")]
+[assembly: InternalsVisibleTo("UniTask.Addressables")]
+[assembly: InternalsVisibleTo("UniTask.DOTween")]
+[assembly: InternalsVisibleTo("UniTask.TextMeshPro")]
---
+++ UniTask/System/Runtime/CompilerServices/AsyncMethodBuilderAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace System.Runtime.CompilerServices
+{
+	// Token: 0x02000003 RID: 3
+	internal sealed class AsyncMethodBuilderAttribute : Attribute
+	{
+		// Token: 0x17000001 RID: 1
+		// (get) Token: 0x06000003 RID: 3 RVA: 0x000020C3 File Offset: 0x000002C3
+		public Type BuilderType { get; }
+
+		// Token: 0x06000004 RID: 4 RVA: 0x000020CB File Offset: 0x000002CB
+		public AsyncMethodBuilderAttribute(Type builderType)
+		{
+			this.BuilderType = builderType;
+		}
+	}
+}
