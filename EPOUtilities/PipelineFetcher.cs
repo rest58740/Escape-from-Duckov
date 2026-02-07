d531783f5c7d3c820dae4caaa9da1e7abe62b233
rest58740 <39267517+rest58740@users.noreply.github.com>
2026-02-07 15:38:19 +0800

    Add files via upload

---
+++ EPOUtilities/PipelineFetcher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace EPOOutline
+{
+	// Token: 0x02000004 RID: 4
+	public static class PipelineFetcher
+	{
+		// Token: 0x17000001 RID: 1
+		// (get) Token: 0x06000005 RID: 5 RVA: 0x00002138 File Offset: 0x00000338
+		public static RenderPipelineAsset CurrentAsset
+		{
+			get
+			{
+				RenderPipelineAsset renderPipelineAsset = QualitySettings.renderPipeline;
+				if (renderPipelineAsset == null)
+				{
+					renderPipelineAsset = GraphicsSettings.renderPipelineAsset;
+				}
+				return renderPipelineAsset;
+			}
+		}
+	}
+}
