9f48d6c2c6d7bfd5a72ba6499a00378e3cb9dc1b
rest58740 <39267517+rest58740@users.noreply.github.com>
2026-02-07 18:00:28 +0800

    Add files via upload

---
+++ mscorlib/System/Threading/_ThreadPoolWaitCallback.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Security;
+
+namespace System.Threading
+{
+	// Token: 0x020002E3 RID: 739
+	internal static class _ThreadPoolWaitCallback
+	{
+		// Token: 0x06002028 RID: 8232 RVA: 0x000755F4 File Offset: 0x000737F4
+		[SecurityCritical]
+		internal static bool PerformWaitCallback()
+		{
+			return ThreadPoolWorkQueue.Dispatch();
+		}
+	}
+}
