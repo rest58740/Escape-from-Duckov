31ffb9fb747f6eeb19b9d545a32975d5f3b75851
rest58740 <39267517+rest58740@users.noreply.github.com>
2026-02-07 14:23:40 +0800

    Add files via upload

---
+++ ALINE/$BurstDirectCallInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Drawing;
+using UnityEngine;
+
+// Token: 0x0200006E RID: 110
+internal static class $BurstDirectCallInitializer
+{
+	// Token: 0x060003E5 RID: 997 RVA: 0x0001357F File Offset: 0x0001177F
+	[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
+	private static void Initialize()
+	{
+		CommandBuilder.Initialize$JobWireMesh_WireMesh_00000105$BurstDirectCall();
+		CommandBuilder.Initialize$JobWireMesh_Execute_00000106$BurstDirectCall();
+		DrawingData.Initialize$BuilderData_AnyBuffersWrittenTo_000002F7$BurstDirectCall();
+		DrawingData.Initialize$BuilderData_ResetAllBuffers_000002F8$BurstDirectCall();
+	}
+}
---
+++ ALINE/__JobReflectionRegistrationOutput__3150089336157158032.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Drawing;
+using Drawing.Examples;
+using Unity.Jobs;
+using UnityEngine;
+
+// Token: 0x0200006D RID: 109
+[DOTSCompilerGenerated]
+internal class __JobReflectionRegistrationOutput__3150089336157158032
+{
+	// Token: 0x060003E3 RID: 995 RVA: 0x00013534 File Offset: 0x00011734
+	public static void CreateJobReflectionData()
+	{
+		try
+		{
+			IJobExtensions.EarlyJobInit<GeometryBuilderJob>();
+			IJobExtensions.EarlyJobInit<PersistentFilterJob>();
+			IJobExtensions.EarlyJobInit<StreamSplitter>();
+			IJobExtensions.EarlyJobInit<BurstExample.DrawingJob>();
+		}
+		catch (Exception ex)
+		{
+			EarlyInitHelpers.JobReflectionDataCreationFailed(ex);
+		}
+	}
+
+	// Token: 0x060003E4 RID: 996 RVA: 0x00013578 File Offset: 0x00011778
+	[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
+	public static void EarlyInit()
+	{
+		__JobReflectionRegistrationOutput__3150089336157158032.CreateJobReflectionData();
+	}
+}
