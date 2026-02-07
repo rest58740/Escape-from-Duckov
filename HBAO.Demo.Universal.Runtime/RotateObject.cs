d579b675db90fe5b9f5f5bb8014625da1341960c
rest58740 <39267517+rest58740@users.noreply.github.com>
2026-02-07 15:58:46 +0800

    Add files via upload

---
+++ HBAO.Demo.Universal.Runtime/RotateObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace HorizonBasedAmbientOcclusion.Universal
+{
+	// Token: 0x02000004 RID: 4
+	public class RotateObject : MonoBehaviour
+	{
+		// Token: 0x06000008 RID: 8 RVA: 0x000021C0 File Offset: 0x000003C0
+		private void Start()
+		{
+		}
+
+		// Token: 0x06000009 RID: 9 RVA: 0x000021C2 File Offset: 0x000003C2
+		private void Update()
+		{
+			base.transform.Rotate(Vector3.up * Time.deltaTime * 15f, Space.World);
+		}
+	}
+}
