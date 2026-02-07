b48f9a727216e227b0b23a806db0a7168c62e002
rest58740 <39267517+rest58740@users.noreply.github.com>
2026-02-07 18:35:59 +0800

    Add files via upload

---
+++ Razer.WyvrnSDK.Runtime/RazerErrors.cs
@@ -0,0 +1,180 @@
+﻿using System;
+
+namespace WyvrnSDK
+{
+	// Token: 0x02000003 RID: 3
+	public class RazerErrors
+	{
+		// Token: 0x06000003 RID: 3 RVA: 0x000020BC File Offset: 0x000002BC
+		public static string GetResultString(int result)
+		{
+			if (result <= 1062)
+			{
+				if (result <= 5)
+				{
+					if (result <= -1)
+					{
+						if (result == -2147467259)
+						{
+							return "RZRESULT_FAILED";
+						}
+						if (result == -1)
+						{
+							return "RZRESULT_INVALID";
+						}
+					}
+					else
+					{
+						if (result == 0)
+						{
+							return "RZRESULT_SUCCESS";
+						}
+						if (result == 5)
+						{
+							return "RZRESULT_ACCESS_DENIED";
+						}
+					}
+				}
+				else if (result <= 50)
+				{
+					if (result == 6)
+					{
+						return "RZRESULT_INVALID_HANDLE";
+					}
+					if (result == 50)
+					{
+						return "RZRESULT_NOT_SUPPORTED";
+					}
+				}
+				else
+				{
+					if (result == 87)
+					{
+						return "RZRESULT_INVALID_PARAMETER";
+					}
+					if (result == 259)
+					{
+						return "RZRESULT_NO_MORE_ITEMS";
+					}
+					if (result == 1062)
+					{
+						return "RZRESULT_SERVICE_NOT_ACTIVE";
+					}
+				}
+			}
+			else if (result <= 1247)
+			{
+				if (result <= 1167)
+				{
+					if (result == 1152)
+					{
+						return "RZRESULT_SINGLE_INSTANCE_APP";
+					}
+					if (result == 1167)
+					{
+						return "RZRESULT_DEVICE_NOT_CONNECTED";
+					}
+				}
+				else
+				{
+					if (result == 1168)
+					{
+						return "RZRESULT_NOT_FOUND";
+					}
+					if (result == 1235)
+					{
+						return "RZRESULT_REQUEST_ABORTED";
+					}
+					if (result == 1247)
+					{
+						return "RZRESULT_ALREADY_INITIALIZED";
+					}
+				}
+			}
+			else if (result <= 4319)
+			{
+				if (result == 4309)
+				{
+					return "RZRESULT_RESOURCE_DISABLED";
+				}
+				if (result == 4319)
+				{
+					return "RZRESULT_DEVICE_NOT_AVAILABLE";
+				}
+			}
+			else
+			{
+				if (result == 5023)
+				{
+					return "RZRESULT_NOT_VALID_STATE";
+				}
+				if (result == 6023)
+				{
+					return "RZRESULT_DLL_NOT_FOUND";
+				}
+				if (result == 6033)
+				{
+					return "RZRESULT_DLL_INVALID_SIGNATURE";
+				}
+			}
+			return result.ToString();
+		}
+
+		// Token: 0x04000001 RID: 1
+		public const int RZRESULT_INVALID = -1;
+
+		// Token: 0x04000002 RID: 2
+		public const int RZRESULT_SUCCESS = 0;
+
+		// Token: 0x04000003 RID: 3
+		public const int RZRESULT_ACCESS_DENIED = 5;
+
+		// Token: 0x04000004 RID: 4
+		public const int RZRESULT_INVALID_HANDLE = 6;
+
+		// Token: 0x04000005 RID: 5
+		public const int RZRESULT_NOT_SUPPORTED = 50;
+
+		// Token: 0x04000006 RID: 6
+		public const int RZRESULT_INVALID_PARAMETER = 87;
+
+		// Token: 0x04000007 RID: 7
+		public const int RZRESULT_SERVICE_NOT_ACTIVE = 1062;
+
+		// Token: 0x04000008 RID: 8
+		public const int RZRESULT_SINGLE_INSTANCE_APP = 1152;
+
+		// Token: 0x04000009 RID: 9
+		public const int RZRESULT_DEVICE_NOT_CONNECTED = 1167;
+
+		// Token: 0x0400000A RID: 10
+		public const int RZRESULT_NOT_FOUND = 1168;
+
+		// Token: 0x0400000B RID: 11
+		public const int RZRESULT_REQUEST_ABORTED = 1235;
+
+		// Token: 0x0400000C RID: 12
+		public const int RZRESULT_ALREADY_INITIALIZED = 1247;
+
+		// Token: 0x0400000D RID: 13
+		public const int RZRESULT_RESOURCE_DISABLED = 4309;
+
+		// Token: 0x0400000E RID: 14
+		public const int RZRESULT_DEVICE_NOT_AVAILABLE = 4319;
+
+		// Token: 0x0400000F RID: 15
+		public const int RZRESULT_NOT_VALID_STATE = 5023;
+
+		// Token: 0x04000010 RID: 16
+		public const int RZRESULT_NO_MORE_ITEMS = 259;
+
+		// Token: 0x04000011 RID: 17
+		public const int RZRESULT_DLL_NOT_FOUND = 6023;
+
+		// Token: 0x04000012 RID: 18
+		public const int RZRESULT_DLL_INVALID_SIGNATURE = 6033;
+
+		// Token: 0x04000013 RID: 19
+		public const int RZRESULT_FAILED = -2147467259;
+	}
+}
---
+++ Razer.WyvrnSDK.Runtime/WyvrnAPI.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using UnityEngine;
+
+namespace WyvrnSDK
+{
+	// Token: 0x02000005 RID: 5
+	public static class WyvrnAPI
+	{
+		// Token: 0x06000005 RID: 5 RVA: 0x00002244 File Offset: 0x00000444
+		static WyvrnAPI()
+		{
+			try
+			{
+				string[] array = new string[]
+				{
+					Path.Combine(Environment.GetFolderPath(38), "Razer Chroma SDK", "bin", "RzChromatic64.dll"),
+					Path.Combine(Environment.GetFolderPath(36), "System32", "RzChromatic64.dll")
+				};
+				for (int i = 0; i < array.Length; i++)
+				{
+					if (!WyvrnAPI.IsProductionVersionAvailable(array[i]))
+					{
+						return;
+					}
+				}
+				WyvrnAPI._sIsChromaticAvailable = true;
+			}
+			catch (Exception ex)
+			{
+				UnityEngine.Debug.LogError(string.Format("The ChromaSDK is not available! Exception={0}", ex));
+			}
+		}
+
+		// Token: 0x06000006 RID: 6 RVA: 0x000022DC File Offset: 0x000004DC
+		private static bool IsProductionVersionAvailable(string fileName)
+		{
+			bool result;
+			try
+			{
+				if (!new FileInfo(fileName).Exists)
+				{
+					result = false;
+				}
+				else
+				{
+					string[] array = FileVersionInfo.GetVersionInfo(fileName).FileVersion.Split(".".ToCharArray());
+					int num;
+					int num2;
+					int num3;
+					int num4;
+					if (array.Length < 4)
+					{
+						result = false;
+					}
+					else if (!int.TryParse(array[0], ref num))
+					{
+						result = false;
+					}
+					else if (!int.TryParse(array[1], ref num2))
+					{
+						result = false;
+					}
+					else if (!int.TryParse(array[2], ref num3))
+					{
+						result = false;
+					}
+					else if (!int.TryParse(array[3], ref num4))
+					{
+						result = false;
+					}
+					else if (num < 2)
+					{
+						result = false;
+					}
+					else if (num == 2 && num2 < 0)
+					{
+						result = false;
+					}
+					else if (num == 2 && num2 == 0 && num3 < 2)
+					{
+						result = false;
+					}
+					else if (num == 2 && num2 == 0 && num3 == 2 && num4 < 0)
+					{
+						result = false;
+					}
+					else
+					{
+						result = true;
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				UnityEngine.Debug.LogError(string.Format("The ChromaSDK is not available! Exception={0}", ex));
+				result = false;
+			}
+			return result;
+		}
+
+		// Token: 0x06000007 RID: 7 RVA: 0x000023D8 File Offset: 0x000005D8
+		public static bool IsWyvrnSDKAvailable()
+		{
+			return WyvrnAPI._sIsChromaticAvailable;
+		}
+
+		// Token: 0x06000008 RID: 8 RVA: 0x000023E0 File Offset: 0x000005E0
+		private static IntPtr GetUnicodeIntPtr(string str)
+		{
+			if (string.IsNullOrEmpty(str))
+			{
+				return IntPtr.Zero;
+			}
+			byte[] bytes = Encoding.Unicode.GetBytes(str + "\0");
+			IntPtr intPtr = Marshal.AllocHGlobal(bytes.Length);
+			Marshal.Copy(bytes, 0, intPtr, bytes.Length);
+			return intPtr;
+		}
+
+		// Token: 0x06000009 RID: 9 RVA: 0x00002426 File Offset: 0x00000626
+		private static void FreeIntPtr(IntPtr lpData)
+		{
+			if (lpData != IntPtr.Zero)
+			{
+				Marshal.FreeHGlobal(lpData);
+			}
+		}
+
+		// Token: 0x0600000A RID: 10 RVA: 0x0000243B File Offset: 0x0000063B
+		public static int CoreInitSDK(ref APPINFOTYPE appInfo)
+		{
+			appInfo.SupportedDevice = 63U;
+			if (!WyvrnAPI._sIsChromaticAvailable)
+			{
+				return -1;
+			}
+			if (WyvrnAPI._sInitialized)
+			{
+				return 0;
+			}
+			int num = WyvrnAPI.PluginCoreInitSDK(ref appInfo);
+			if (num == 0)
+			{
+				WyvrnAPI._sInitialized = true;
+			}
+			return num;
+		}
+
+		// Token: 0x0600000B RID: 11 RVA: 0x00002468 File Offset: 0x00000668
+		public static int CoreSetEventName(string name)
+		{
+			if (!WyvrnAPI._sIsChromaticAvailable)
+			{
+				return -1;
+			}
+			if (!WyvrnAPI._sInitialized)
+			{
+				return -1;
+			}
+			IntPtr unicodeIntPtr = WyvrnAPI.GetUnicodeIntPtr(name);
+			int result = WyvrnAPI.PluginCoreSetEventName(unicodeIntPtr);
+			WyvrnAPI.FreeIntPtr(unicodeIntPtr);
+			return result;
+		}
+
+		// Token: 0x0600000C RID: 12 RVA: 0x0000249A File Offset: 0x0000069A
+		public static int CoreUnInit()
+		{
+			if (!WyvrnAPI._sIsChromaticAvailable)
+			{
+				return 0;
+			}
+			if (!WyvrnAPI._sInitialized)
+			{
+				return 0;
+			}
+			int num = WyvrnAPI.PluginCoreUnInit();
+			if (num == 0)
+			{
+				WyvrnAPI._sInitialized = false;
+			}
+			return num;
+		}
+
+		// Token: 0x0600000D RID: 13
+		[DllImport("RzChromatic64", CallingConvention = 2)]
+		private static extern int PluginCoreInitSDK(ref APPINFOTYPE appInfo);
+
+		// Token: 0x0600000E RID: 14
+		[DllImport("RzChromatic64", CallingConvention = 2)]
+		private static extern int PluginCoreSetEventName(IntPtr name);
+
+		// Token: 0x0600000F RID: 15
+		[DllImport("RzChromatic64", CallingConvention = 2)]
+		private static extern int PluginCoreUnInit();
+
+		// Token: 0x0400001A RID: 26
+		private const string DLL_NAME = "RzChromatic64";
+
+		// Token: 0x0400001B RID: 27
+		private static bool _sIsChromaticAvailable = false;
+
+		// Token: 0x0400001C RID: 28
+		private static bool _sInitialized;
+	}
+}
