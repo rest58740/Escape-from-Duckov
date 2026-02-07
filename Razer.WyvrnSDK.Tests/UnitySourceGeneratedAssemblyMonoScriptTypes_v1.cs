9ad5ee1a62221b6e3f46365ab72bc070689a67c2
rest58740 <39267517+rest58740@users.noreply.github.com>
2026-02-07 18:36:33 +0800

    Add files via upload

---
+++ Razer.WyvrnSDK.Tests/UnitySourceGeneratedAssemblyMonoScriptTypes_v1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+// Token: 0x02000003 RID: 3
+[CompilerGenerated]
+[EditorBrowsable(EditorBrowsableState.Never)]
+[GeneratedCode("Unity.MonoScriptGenerator.MonoScriptInfoGenerator", null)]
+internal class UnitySourceGeneratedAssemblyMonoScriptTypes_v1
+{
+	// Token: 0x06000017 RID: 23 RVA: 0x000024D8 File Offset: 0x000006D8
+	[MethodImpl(256)]
+	private static UnitySourceGeneratedAssemblyMonoScriptTypes_v1.MonoScriptData Get()
+	{
+		UnitySourceGeneratedAssemblyMonoScriptTypes_v1.MonoScriptData result = default(UnitySourceGeneratedAssemblyMonoScriptTypes_v1.MonoScriptData);
+		result.FilePathsData = new byte[]
+		{
+			0,
+			0,
+			0,
+			1,
+			0,
+			0,
+			0,
+			71,
+			92,
+			76,
+			105,
+			98,
+			114,
+			97,
+			114,
+			121,
+			92,
+			80,
+			97,
+			99,
+			107,
+			97,
+			103,
+			101,
+			67,
+			97,
+			99,
+			104,
+			101,
+			92,
+			99,
+			111,
+			109,
+			46,
+			114,
+			97,
+			122,
+			101,
+			114,
+			46,
+			119,
+			121,
+			118,
+			114,
+			110,
+			115,
+			100,
+			107,
+			64,
+			57,
+			54,
+			97,
+			49,
+			102,
+			52,
+			49,
+			100,
+			53,
+			57,
+			92,
+			84,
+			101,
+			115,
+			116,
+			115,
+			92,
+			71,
+			97,
+			109,
+			101,
+			83,
+			97,
+			109,
+			112,
+			108,
+			101,
+			46,
+			99,
+			115
+		};
+		result.TypesData = new byte[]
+		{
+			0,
+			0,
+			0,
+			0,
+			11,
+			124,
+			71,
+			97,
+			109,
+			101,
+			83,
+			97,
+			109,
+			112,
+			108,
+			101
+		};
+		result.TotalFiles = 1;
+		result.TotalTypes = 1;
+		result.IsEditorOnly = false;
+		return result;
+	}
+
+	// Token: 0x02000006 RID: 6
+	private struct MonoScriptData
+	{
+		// Token: 0x04000009 RID: 9
+		public byte[] FilePathsData;
+
+		// Token: 0x0400000A RID: 10
+		public byte[] TypesData;
+
+		// Token: 0x0400000B RID: 11
+		public int TotalTypes;
+
+		// Token: 0x0400000C RID: 12
+		public int TotalFiles;
+
+		// Token: 0x0400000D RID: 13
+		public bool IsEditorOnly;
+	}
+}
