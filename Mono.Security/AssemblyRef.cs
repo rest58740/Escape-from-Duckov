3cf5be8ccda90c20ec5b73d0d386bfe8083cea9d
rest58740 <39267517+rest58740@users.noreply.github.com>
2026-02-07 16:15:58 +0800

    Add files via upload

---
+++ Mono.Security/AssemblyRef.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+// Token: 0x02000002 RID: 2
+internal static class AssemblyRef
+{
+	// Token: 0x04000001 RID: 1
+	internal const string SystemConfiguration = "System.Configuration, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a";
+
+	// Token: 0x04000002 RID: 2
+	internal const string System = "System, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35";
+
+	// Token: 0x04000003 RID: 3
+	public const string EcmaPublicKey = "b77a5c561934e089";
+
+	// Token: 0x04000004 RID: 4
+	public const string FrameworkPublicKeyFull = "00000000000000000400000000000000";
+
+	// Token: 0x04000005 RID: 5
+	public const string FrameworkPublicKeyFull2 = "00000000000000000400000000000000";
+
+	// Token: 0x04000006 RID: 6
+	public const string MicrosoftPublicKey = "b03f5f7f11d50a3a";
+
+	// Token: 0x04000007 RID: 7
+	public const string MicrosoftJScript = "Microsoft.JScript, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a";
+
+	// Token: 0x04000008 RID: 8
+	public const string MicrosoftVSDesigner = "Microsoft.VSDesigner, Version=0.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a";
+
+	// Token: 0x04000009 RID: 9
+	public const string SystemData = "System.Data, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089";
+
+	// Token: 0x0400000A RID: 10
+	public const string SystemDesign = "System.Design, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a";
+
+	// Token: 0x0400000B RID: 11
+	public const string SystemDrawing = "System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a";
+
+	// Token: 0x0400000C RID: 12
+	public const string SystemWeb = "System.Web, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a";
+
+	// Token: 0x0400000D RID: 13
+	public const string SystemWebExtensions = "System.Web.Extensions, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35";
+
+	// Token: 0x0400000E RID: 14
+	public const string SystemWindowsForms = "System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089";
+}
