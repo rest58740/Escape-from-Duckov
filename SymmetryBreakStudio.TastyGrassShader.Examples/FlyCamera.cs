70a368d94d7a7687d991e5769bf7040b40e3d0fa
rest58740 <39267517+rest58740@users.noreply.github.com>
2026-02-07 18:52:11 +0800

    Add files via upload

---
+++ SymmetryBreakStudio.TastyGrassShader.Examples/FlyCamera.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using UnityEngine;
+
+namespace SymmetryBreakStudio.TastyGrassShader.Example
+{
+	// Token: 0x02000006 RID: 6
+	public class FlyCamera : MonoBehaviour
+	{
+		// Token: 0x06000016 RID: 22 RVA: 0x000026C9 File Offset: 0x000008C9
+		private void Start()
+		{
+			this._rotation = base.transform.localRotation;
+		}
+
+		// Token: 0x06000017 RID: 23 RVA: 0x000026DC File Offset: 0x000008DC
+		private void Update()
+		{
+			Vector3 vector = Vector3.zero;
+			if (Input.GetKey(KeyCode.W))
+			{
+				vector += Vector3.forward;
+			}
+			if (Input.GetKey(KeyCode.S))
+			{
+				vector += Vector3.back;
+			}
+			if (Input.GetKey(KeyCode.A))
+			{
+				vector -= Vector3.right;
+			}
+			if (Input.GetKey(KeyCode.D))
+			{
+				vector += Vector3.right;
+			}
+			if (Input.GetKey(KeyCode.Q))
+			{
+				vector -= Vector3.up;
+			}
+			if (Input.GetKey(KeyCode.E))
+			{
+				vector += Vector3.up;
+			}
+			vector = vector.normalized;
+			this._movement = Vector3.SmoothDamp(this._movement, vector, ref this._movementVelocity, this.smoothing);
+			base.transform.Translate(this._movement * (this.speed * Time.deltaTime), Space.Self);
+			float x = 0f;
+			float y = 0f;
+			if (Input.GetMouseButton(1))
+			{
+				x = Input.GetAxis("Mouse X");
+				y = Input.GetAxis("Mouse Y");
+			}
+			this._mouse = Vector2.SmoothDamp(this._mouse, new Vector2(x, y), ref this._mouseVelocity, this.mouseSmoothing);
+			Vector3 vector2 = this._rotation.eulerAngles;
+			vector2 += new Vector3(-this._mouse.y, this._mouse.x, 0f) * (this.mouseSpeed * Time.deltaTime);
+			float x2 = vector2.x;
+			if (x2 <= 90f && x2 >= 0f)
+			{
+				vector2.x = Mathf.Clamp(vector2.x, 0f, 90f);
+			}
+			if (vector2.x >= 270f)
+			{
+				vector2.x = Mathf.Clamp(vector2.x, 270f, 360f);
+			}
+			this._rotation.eulerAngles = vector2;
+			Quaternion rotation = this._rotation;
+			Vector3 eulerAngles = rotation.eulerAngles;
+			eulerAngles.z = 0f;
+			base.transform.localRotation = Quaternion.Euler(eulerAngles);
+		}
+
+		// Token: 0x04000016 RID: 22
+		public float speed;
+
+		// Token: 0x04000017 RID: 23
+		public float smoothing;
+
+		// Token: 0x04000018 RID: 24
+		public float mouseSpeed;
+
+		// Token: 0x04000019 RID: 25
+		public float mouseSmoothing;
+
+		// Token: 0x0400001A RID: 26
+		private Vector2 _mouse;
+
+		// Token: 0x0400001B RID: 27
+		private Vector2 _mouseVelocity;
+
+		// Token: 0x0400001C RID: 28
+		private Vector3 _movement;
+
+		// Token: 0x0400001D RID: 29
+		private Vector3 _movementVelocity;
+
+		// Token: 0x0400001E RID: 30
+		private Quaternion _rotation;
+	}
+}
---
+++ SymmetryBreakStudio.TastyGrassShader.Examples/TgsCustomInstanceExample.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using SymmetryBreakStudio.TastyGrassShader;
+using UnityEngine;
+
+// Token: 0x02000002 RID: 2
+[ExecuteInEditMode]
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class TgsCustomInstanceExample : MonoBehaviour
+{
+	// Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
+	private void OnEnable()
+	{
+		this._instance.Hide = false;
+		this.BakeInstance();
+	}
+
+	// Token: 0x06000002 RID: 2 RVA: 0x00002064 File Offset: 0x00000264
+	private void OnValidate()
+	{
+		this.BakeInstance();
+	}
+
+	// Token: 0x06000003 RID: 3 RVA: 0x0000206C File Offset: 0x0000026C
+	private void BakeInstance()
+	{
+		if (this.settings.preset == null)
+		{
+			return;
+		}
+		if (this.windSettings == null)
+		{
+			return;
+		}
+		Mesh sharedMesh = base.GetComponent<MeshFilter>().sharedMesh;
+		Matrix4x4 localToWorldMatrix = base.transform.localToWorldMatrix;
+		Bounds bounds = base.GetComponent<MeshRenderer>().bounds;
+		TgsInstance.TgsInstanceRecipe bakeParameters = TgsInstance.TgsInstanceRecipe.BakeFromMesh(localToWorldMatrix, this.settings, sharedMesh, bounds);
+		this._instance.SetBakeParameters(bakeParameters);
+		this._instance.MarkGeometryDirty();
+		this._instance.UsedWindSettings = this.windSettings;
+		this._instance.MarkMaterialDirty();
+	}
+
+	// Token: 0x06000004 RID: 4 RVA: 0x00002100 File Offset: 0x00000300
+	private void OnDisable()
+	{
+		this._instance.Hide = true;
+	}
+
+	// Token: 0x06000005 RID: 5 RVA: 0x0000210E File Offset: 0x0000030E
+	private void OnDestroy()
+	{
+		this._instance.Release();
+	}
+
+	// Token: 0x04000001 RID: 1
+	private TgsInstance _instance = new TgsInstance();
+
+	// Token: 0x04000002 RID: 2
+	public TgsPreset.Settings settings = TgsPreset.Settings.GetDefault();
+
+	// Token: 0x04000003 RID: 3
+	public TgsWindSettings windSettings;
+}
