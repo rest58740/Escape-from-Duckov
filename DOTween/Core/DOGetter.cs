0dfc581d9326b5332902b086939cea895217639f
rest58740 <39267517+rest58740@users.noreply.github.com>
2026-02-08 19:38:02 +0800

    Delete DOTween directory

--- DOTween/Core/DOGetter.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DG.Tweening.Core
-{
-	// Token: 0x0200004C RID: 76
-	// (Invoke) Token: 0x060002A5 RID: 677
-	public delegate T DOGetter<out T>();
-}
--- DOTween/Core/DOSetter.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DG.Tweening.Core
-{
-	// Token: 0x0200004D RID: 77
-	// (Invoke) Token: 0x060002A9 RID: 681
-	public delegate void DOSetter<in T>(T pNewValue);
-}
