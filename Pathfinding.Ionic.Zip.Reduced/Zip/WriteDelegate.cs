cff883e7cc7d6483f36c8dba9eb1c6903e488caa
rest58740 <39267517+rest58740@users.noreply.github.com>
2026-02-08 19:36:49 +0800

    Delete Pathfinding.Ionic.Zip.Reduced directory

--- Pathfinding.Ionic.Zip.Reduced/Zip/WriteDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.IO;
-
-namespace Pathfinding.Ionic.Zip
-{
-	// Token: 0x02000004 RID: 4
-	// (Invoke) Token: 0x06000009 RID: 9
-	public delegate void WriteDelegate(string entryName, Stream stream);
-}
