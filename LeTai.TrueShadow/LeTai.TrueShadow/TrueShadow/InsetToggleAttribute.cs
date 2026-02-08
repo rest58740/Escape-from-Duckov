3d921158dbae40493bb836de52b178d497f3df10
rest58740 <39267517+rest58740@users.noreply.github.com>
2026-02-08 19:39:39 +0800

    Delete LeTai.TrueShadow directory

--- LeTai.TrueShadow/LeTai.TrueShadow/TrueShadow/InsetToggleAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace LeTai.TrueShadow
-{
-	// Token: 0x0200001E RID: 30
-	public class InsetToggleAttribute : ToggleButtonsAttribute
-	{
-	}
-}
--- LeTai.TrueShadow/LeTai.TrueShadow/TrueShadow/PluginInterfaces/ITrueShadowCustomHashProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace LeTai.TrueShadow.PluginInterfaces
-{
-	// Token: 0x02000027 RID: 39
-	public interface ITrueShadowCustomHashProvider
-	{
-	}
-}
