1a67c7d60e087bb8abd0f3b4d52547242b33be25
rest58740 <39267517+rest58740@users.noreply.github.com>
2026-02-08 19:39:25 +0800

    Delete LeTai.TranslucentImage.UniversalRP directory

--- LeTai.TranslucentImage.UniversalRP/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-using System.Diagnostics;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: InternalsVisibleTo("LeTai.TranslucentImage.UniversalRP.Editor")]
